4281a63af66bdf7460eeb3043ce6ab02591a2fde
alexander1914 <alexander.oliveira99@gmail.com>
2026-05-28 19:33:01 +0100

    [Set up project to work JPA] - I created a project to review many content JPA more deep today

---
+++ src/main/java/com/github/alexander1914/spring_jpa_review/SpringJpaReviewApplication.java
@@ -0,0 +1,13 @@
+package com.github.alexander1914.spring_jpa_review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaReviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/alexander1914/spring_jpa_review/SpringJpaReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alexander1914.spring_jpa_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
